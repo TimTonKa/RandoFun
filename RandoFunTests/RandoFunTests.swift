7bc619825484c38ce7291a681c9c23a918d7494a
timzheng <timzheng@truetel.com>
2025-06-05 11:10:34 +0800

    initial commit

---
+++ RandoFunTests/RandoFunTests.swift
@@ -0,0 +1,17 @@
+//
+//  RandoFunTests.swift
+//  RandoFunTests
+//
+//  Created by Tim Tseng on 2025/6/5.
+//
+
+import Testing
+@testable import RandoFun
+
+struct RandoFunTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
